eec8294becf0346356ebde5bd63bb44e3b72a8f5
sydcanem <icqhv.santos@gmail.com>
2013-09-03 02:38:37 +0800

    Remove strays

--- README.md
+++ README.md
@@ -2,7 +2,3 @@ quizzer
 =======
 
 Quizzer application.
-
-Simple usage (this may change tomorrow)
-
-`curl -d "process.nextTick(function () { done(true); })" localhost:9000`
--- server.js
+++ server.js
@@ -32,16 +32,3 @@ server.listen(config.app_port, config.app_host, function () {
 	var addr = server.address();
 	console.log('Quizzer listening on http://' + addr.address + ':' + addr.port);
 });
-
-function testConnection(snippet) {
-	var code = "".concat(
-				'var MongoClient = require("mongodb").MongoClient;',
-				'var conn = new MongoClient();',
-				'conn.connect("mongodb://localhost:27017/test", function (err, db) {',
-				'	if (err) return console.log(err);',
-				'	console.log("Connection established!");',
-				snippet,
-				'});'
-			);
-	return code;
-}
