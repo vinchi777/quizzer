7a1e6459d19ac80c98643c00816e4d1aef3cf0d3
sydcanem <icqhv.santos@gmail.com>
2013-09-05 23:32:21 +0800

    Some changes to many to document. At least it's working fine :).

--- training.js
+++ training.js
@@ -1,14 +1,50 @@
 var MongoClient = require('mongodb').MongoClient;
 var _ = require('underscore');
 
-MongoClient.connect('mongodb://localhost:27017/school', function (err, db){
-	if (err) return console.log(err);
+function done(doc) {
+	console.log(doc);
+}
 
-	var studs = db.collection('students');
-	studs.count(function (num) {
-		console.log(num);
+MongoClient.connect('mongodb://localhost:27017/jsantos', function (err, db){
+	if (err) return console.log(err);
+	db.authenticate('quizzer', 'quizzer', {authdb: 'admin'}, function (err, result) {
+		console.log('result: ', result);
+		var blogs = db.collection('blogs');
+		blogs.remove({}, {w: 0});
+		blogs.insert({
+		  "_id": 1,
+		  "content" : "...",
+		  "comments" : [
+		    {
+		      "author" : "joe",
+		      "score" : 3,
+		      "comment" : "nice post"
+		    },
+		    {
+		      "author" : "mary",
+		      "score" : 6,
+		      "comment" : "terrible post"
+		    }
+		  ]
+		}, function (err, doc) {
+			if (err) done(err);
+			done(doc[0]);
+		});
 	});
-	console.log(studs.count());
+
+	// var reviews = db.collection('reviews');
+
+	// reviews.insert({
+	// 	"restaurant" : "Le Cirque",
+	// 	"review" : "Hamburgers were overpriced.",
+	// 	"tags" : [],
+	// 	"garbage": Array(100).join('.')
+	// }, function (err, doc) {
+	// 	if (err) return console.log(err);
+	// 	console.log(doc[0]);
+	// 	console.log('Buffer size: ', Buffer.byteLength(doc[0].garbage, 'utf8'));
+	// });
+	// var studs = db.collection('students');
 	// studs.find().each(function (err, doc) {
 	// 	if (doc) {
 	// 		var scores = doc.scores;
